4d8b6e7a5a0f557c55c06c04dd5e2e8097514ae7
Niranjan Paranjape <niranjangp@gmail.com>
2021-10-19 11:16:32 +0800

    Capture missing dot-files

--- install.sh
+++ install.sh
@@ -4,3 +4,5 @@ stow -t $HOME --dotfiles brew
 stow -t $HOME --dotfiles emacs
 stow -t $HOME --dotfiles ruby
 stow -t $HOME --dotfiles git
+stow -t $HOME --dotfiles shell
+stow -t $HOME --dotfiles tmux
